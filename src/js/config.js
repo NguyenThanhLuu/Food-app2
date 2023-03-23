2e62696c1179d316122c20aebce162a96b38c0b0
nguyen thanh luu <nguyenthanhluu@gmail.com>
2023-03-23 20:12:12 +0700

    First commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '80069bba-7b48-4c36-853d-5cbd0932e3e5';
+export const MODAL_CLOSE_SEC = 2.5;
